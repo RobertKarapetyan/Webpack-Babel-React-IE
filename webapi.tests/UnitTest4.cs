db4dd013f42bf88f69f0a93c5d7eda3621504b73
engineer0001 <engineernumber001@gmail.com>
2019-02-17 11:27:47 -0800

    Add Unit Test 4

---
+++ webapi.tests/UnitTest4.cs
@@ -0,0 +1,32 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace webapi.tests
+{
+    [TestClass]
+    public class UnitTest4
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(true, true);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(true, true);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(true, true);
+        }
+
+         [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+}
